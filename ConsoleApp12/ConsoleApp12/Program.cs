2f72d158932956c70e1c86bc715f9bf7f10aa1ae
Kamiface <40615248+Kamiface@users.noreply.github.com>
2018-07-30 17:50:23 -0700

    update
    fixed

--- ConsoleApp12/ConsoleApp12/Program.cs
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -53,13 +53,7 @@ namespace ConsoleApp12
 
 
 
-            //1. Create a class. In that class, create a void method that takes two integers as parameters.Have the method do a math operation on the first integer and display the second integer to the screen.
 
-            //2. In the Main() method of the console app, instantiate the class.
-
-            //3. Call the method in the class, passing in two numbers.
-
-            //4. Call the method in the class, specifying the parameters by name.
 
 
         }
