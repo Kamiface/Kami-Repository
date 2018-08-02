98eb519cd2992840bb70f4c1cdeb8e724b3c098e
Kamiface <40615248+Kamiface@users.noreply.github.com>
2018-08-01 18:59:47 -0700

    Overloaded
    Operators

---
+++ ConsoleApp4/ConsoleApp4/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Employee : Person, IQuittable
+    {
+
+        // 1. Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
+
+        public int ID { get; set; }
+
+        public static Employee operator== (Employee Id)
+        {
+            bool IdEqual = 
+        }
+
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: [{0} {1}]", firstName, lastName);
+        }
+
+       
+        public void Quit()
+        {
+            Console.WriteLine("The Employee Quits!");
+        }
+
+       
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
+            Employee employee = new Employee();
+
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            //employee.SayName();
+
+            //IQuittable newEmployee = new Employee();
+            
+            
+            //newEmployee.Quit();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
