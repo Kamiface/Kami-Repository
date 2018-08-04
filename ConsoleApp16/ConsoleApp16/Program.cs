657332c2dd6bc797e8906f192ca4b2ba8507fcb4
Kamiface <40615248+Kamiface@users.noreply.github.com>
2018-08-03 22:13:49 -0700

    Structs!
    In-struct-ional!

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1.Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+            Number number = new Number() { Amount = 52.3564m };
+
+            //3. Print this amount to the console.
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
